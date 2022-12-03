c7e45157d4c82d2006cf3a8fc96c812b602a6c6e
Chanwookim <chanwookim@me.com>
2022-12-04 07:08:35 +0900

    #1-init project

---
+++ src/main/java/com/example/payheretest/PayhereTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.payheretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayhereTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayhereTestApplication.class, args);
+    }
+
+}

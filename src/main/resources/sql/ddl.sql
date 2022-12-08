504ba09736d9ff2068cb07f9bb3bcef8eee0547d
Chanwookim <chanwookim@me.com>
2022-12-08 10:55:55 +0900

    #6 ddl update

--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -1,28 +1,38 @@
-create table users
-(
-    id              bigint auto_increment
-        primary key,
-    created_at      datetime(6)  null,
-    deleted         bit          not null,
-    deleted_at      datetime(6)  null,
-    updated_at      datetime(6)  null,
-    email           varchar(255) null,
-    hashed_password varchar(255) null,
-    name            varchar(255) null
-);
 
-create table money_book
-(
-    id         bigint auto_increment
-        primary key,
-    created_at datetime(6)  null,
-    deleted    bit          not null,
-    deleted_at datetime(6)  null,
-    updated_at datetime(6)  null,
-    memo       varchar(255) null,
-    money      bigint       null,
-    user_id    bigint       null,
-    constraint FKh5ndt01gs0beljrgw6hwc836w
-        foreign key (user_id) references users (id)
-);
+alter table money_book
+drop
+foreign key FKh5ndt01gs0beljrgw6hwc836w
 
+drop table if exists money_book
+
+drop table if exists users
+
+create table money_book (
+                            id bigint not null auto_increment,
+                            created_at datetime(6),
+                            deleted bit not null,
+                            deleted_at datetime(6),
+                            updated_at datetime(6),
+                            memo varchar(255),
+                            money bigint,
+                            user_id bigint,
+                            primary key (id)
+) engine=InnoDB
+
+create table users (
+                       id bigint not null auto_increment,
+                       created_at datetime(6),
+                       deleted bit not null,
+                       deleted_at datetime(6),
+                       updated_at datetime(6),
+                       email varchar(255),
+                       expired_at bigint,
+                       hashed_password varchar(255),
+                       name varchar(255),
+                       primary key (id)
+) engine=InnoDB
+
+alter table money_book
+    add constraint FKh5ndt01gs0beljrgw6hwc836w
+        foreign key (user_id)
+            references users (id)

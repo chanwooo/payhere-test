1594e36618653fe80917dbefa3eafdf5519672f9
Chanwookim <chanwookim@me.com>
2022-12-08 19:59:44 +0900

    #27 - 코드 정리  - ddl fk 이름 변경

--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -1,23 +1,11 @@
 
 alter table money_book
 drop
-foreign key FKh5ndt01gs0beljrgw6hwc836w
+foreign key fk_money_book_user_id_users_id
 
 drop table if exists money_book
 
 drop table if exists users
-
-create table money_book (
-                            id bigint not null auto_increment,
-                            created_at datetime(6),
-                            deleted bit not null,
-                            deleted_at datetime(6),
-                            updated_at datetime(6),
-                            memo varchar(255),
-                            money bigint,
-                            user_id bigint,
-                            primary key (id)
-) engine=InnoDB
 
 create table users (
                        id bigint not null auto_increment,
@@ -32,7 +20,19 @@ create table users (
                        primary key (id)
 ) engine=InnoDB
 
+create table money_book (
+                            id bigint not null auto_increment,
+                            created_at datetime(6),
+                            deleted bit not null,
+                            deleted_at datetime(6),
+                            updated_at datetime(6),
+                            memo varchar(255),
+                            money bigint,
+                            user_id bigint,
+                            primary key (id)
+) engine=InnoDB
+
 alter table money_book
-    add constraint FKh5ndt01gs0beljrgw6hwc836w
+    add constraint fk_money_book_user_id_users_id
         foreign key (user_id)
             references users (id)

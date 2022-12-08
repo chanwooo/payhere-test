5977fdbdc33955072ca4258c03948d5bb0b59189
Chanwookim <chanwookim@me.com>
2022-12-09 04:19:56 +0900

    #30 - put, patch request 분리

--- src/main/java/com/example/payheretest/domain/request/MoneyBookPutUpdateRequest.java
+++ src/main/java/com/example/payheretest/domain/request/MoneyBookPutUpdateRequest.java
@@ -8,18 +8,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
-@Schema(description = "부분 수정 요청, 일부 필드만 요청시 기존값은 그대로두고 변경된 부분만 수정합니다.")
+@Schema(description = "수정 요청, 모든 필드가 유효해야합니다.(Not Null)")
 @Data
 @Setter(AccessLevel.PROTECTED)
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class MoneyBookPatchUpdateRequest {
+public class MoneyBookPutUpdateRequest {
 
     @PositiveOrZero
+    @NotNull
     private Long money;
 
+    @NotNull
     private String memo;
 }
